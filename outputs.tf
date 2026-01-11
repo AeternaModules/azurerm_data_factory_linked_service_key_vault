fd9d83da2ee69b37412cc2ccbfe7210f6930b44e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:44:55 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_key_vaults" {
+  description = "All data_factory_linked_service_key_vault resources"
+  value       = azurerm_data_factory_linked_service_key_vault.data_factory_linked_service_key_vaults
+}
+
